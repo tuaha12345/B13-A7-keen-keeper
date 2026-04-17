e7a38847f77dcb6ce25152783f986dcc0aa767cb
Syeed md tuaha <syeedmdtuaha@gmail.com>
2026-04-17 17:49:00 +0600

    footer responseive

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="footer bg-[#344D3F] text-white py-5">
          <div className="w-full my-5">
             <div className="flex flex-col justify-center items-center gap-7  w-11/12 mx-auto my-7">
-                <h1 className='font-bold text-[50px] text-center'>KeenKeeper</h1>
+                <h1 className='font-bold text-3xl md:text-[50px] text-center'>KeenKeeper</h1>
                 <p className='text-center'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
                 <div className='flex flex-col gap-3'>
                     <h3 className='font-semibold text-[24px] text-center'>Social Links</h3>
@@ -19,9 +19,9 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row justify-between items-center mx-auto w-10/12 border-t border-[#6b8a75] pt-5 ">
+            <div className="flex flex-col md:flex-row justify-between items-center mx-auto w-10/12 border-t border-[#6b8a75] pt-5 ">
               <p>© 2026 KeenKeeper. All rights reserved.</p>
-              <ul className='flex gap-7'>
+              <ul className='flex gap-7 mt-5 md:mt-0'>
                 <li>Privacy Policy</li>
                 <li>Terms of Service</li>
                 <li>Cookies</li>

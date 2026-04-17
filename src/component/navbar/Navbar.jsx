d3d47186a3acc2bb9ff9ebb6388fead8719c6bc2
Syeed md tuaha <syeedmdtuaha@gmail.com>
2026-04-17 17:57:36 +0600

    Navbar responsive

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import "./navbar.css"
 const Navbar = () => {
     const buttonList=(
         <>
-             <div className="navbar-end flex gap-2">
+             <div className="navbar-end flex gap-2 justify-center md:justify-end">
                 <NavLink to={"/"} className="btn border-none"><RiHome2Line />Home</NavLink>
                 <NavLink to={"/timeline"} className="btn border-none"><IoMdTime  />Timeline</NavLink>
                 <NavLink to={"/stats"} className="btn border-none"><ImStatsDots />Stats</NavLink>
@@ -21,8 +21,8 @@ const Navbar = () => {
     
   return (
     
-    <div className="navbar bg-[#F9FAFC] shadow-sm px-15">
-      <div className="navbar-start">
+    <div className="navbar bg-[#F9FAFC] shadow-sm px-15 flex flex-col md:flex-row">
+      <div className="navbar-start mb-5 md:mb-0">
         <Link to={"/"} >
         <a className="text-2xl font-bold">Keen<span className="text-[#344D3F]">Keeper</span></a>
         </Link>
